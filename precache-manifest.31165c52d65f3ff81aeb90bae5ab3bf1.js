feedb62bef4455f3943966501144bd5b16302189
Mohamed BAYOUDH <mohamed.bayoudh@socrate.education>
2023-01-16 03:36:47 +0100

    Updates

---
+++ precache-manifest.31165c52d65f3ff81aeb90bae5ab3bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7574be9ee683f216bc9",
+    "url": "/static/css/main.c6a84f57.chunk.css"
+  },
+  {
+    "revision": "a7574be9ee683f216bc9",
+    "url": "/static/js/main.450a7e3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53cd578f8c044bd2d264",
+    "url": "/static/css/2.0b6337b2.chunk.css"
+  },
+  {
+    "revision": "53cd578f8c044bd2d264",
+    "url": "/static/js/2.d81ff235.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5e2c7e676dc7ae6048298297bafbe9bc",
+    "url": "/index.html"
+  }
+];
